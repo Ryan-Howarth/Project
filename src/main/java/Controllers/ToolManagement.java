8133dcf2961f003180f279dc25a338ecdcebba0d
Ryan-Howarth <88491@farnborough.ac.uk>
2019-11-04 11:00:41 +0000

    End of Phase 2.

--- src/main/java/Controllers/ToolManagement.java
+++ src/main/java/Controllers/ToolManagement.java
@@ -1,58 +1,95 @@
 package Controllers;
 
 import Server.Main;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.sql.ResultSet;
 import java.sql.PreparedStatement;
 
+@Path("Tools/")
 public class ToolManagement {
-    public static void addTool(Integer toolID, String toolName) {
+    @POST
+    @Path("addTool")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String addTool(@FormDataParam("toolID")Integer toolID,
+                          @FormDataParam("toolName")String toolName) {
+
         try {
+            if (toolID == null || toolName == null) {
+                throw new Exception("One or more form data parameters are missing in the HTTP request.");
+            }
             PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Tools(ToolID, ToolName) VALUES (?, ?)");
             ps.setInt(1, toolID);
             ps.setString(2, toolName);
             ps.executeUpdate();
+            return "{\"status\": \"OK\"}";
 
         } catch (Exception exception) {
             System.out.println("Database disconnection error: " + exception.getMessage());
+            return "{\"error\": \"Unable to create new item, please see server console for more info.\"}";
         }
     }
 
-    public static void readTool() {
+    @GET
+    @Path("list")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String readTool() {
+        System.out.println("thing/list");
+        JSONArray list = new JSONArray();
         try {
 
             PreparedStatement ps = Main.db.prepareStatement("SELECT ToolName FROM Tools");
             ResultSet results = ps.executeQuery();
             while (results.next()) {
-                String toolName = results.getString(1);
-                System.out.println(toolName);
+                JSONObject item = new JSONObject();
+                item.put("name", results.getString(1));
+                list.add(item);
             }
+            return list.toString();
 
 
         } catch (Exception exception) {
             System.out.println("Database disconnection error: " + exception.getMessage());
+            return "{\"error\": \"Unable to list items, please see server console for more info.\"}";
         }
     }
 
-    public static void updateTool(String toolName, Integer toolID) {
+    @POST
+    @Path("updateTool")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String updateTool(@FormDataParam("toolName")String toolName,@FormDataParam("toolID") Integer toolID) {
         try {
             PreparedStatement ps = Main.db.prepareStatement("UPDATE Tools SET ToolName = ? WHERE ToolID = ?");
             ps.setString(1, toolName);
             ps.setInt(2, toolID);
             ps.executeUpdate();
+            return "{\"status\": \"OK\"}";
 
         } catch (Exception exception) {
             System.out.println("Database disconnection error: " + exception.getMessage());
+            return "{\"error\": \"Unable to delete item, please see server console for more info.\"}";
         }
     }
-    public static void deleteTool(Integer toolID) {
+    @POST
+    @Path("deleteTool")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String deleteTool(@FormDataParam("toolID")Integer toolID) {
         try {
             PreparedStatement ps = Main.db.prepareStatement("DELETE FROM Tools WHERE ToolID = ?");
             ps.setInt(1, toolID);
             ps.executeUpdate();
+            return "{\"status\": \"OK\"}";
 
         } catch (Exception exception) {
             System.out.println("Database disconnection error: " + exception.getMessage());
+            return "{\"error\": \"Unable to delete item, please see server console for more info.\"}";
         }
     }
 }
